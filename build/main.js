47a952152f8f0b89081f0d5e2fa1fd3173d4ca5a
Mart Linger <martlinger@gmail.com>
2022-04-13 23:12:13 +0200

    Add TypeScript

--- build/main.js
+++ build/main.js
@@ -1,46 +1,44 @@
-module.exports.loop = () => {
-    for (const i in Game.creeps) {
-        const creep = Game.creeps[i];
-        const closestResource = creep.pos.findClosestByPath(FIND_SOURCES);
-        
+module.exports.loop = function () {
+    for (var i in Game.creeps) {
+        var creep = Game.creeps[i];
+        var closestResource = creep.pos.findClosestByPath(FIND_SOURCES);
+        // @ts-expect-error
         if (creep.store.getFreeCapacity() > 0 && !creep.memory.working) {
             if (creep.harvest(closestResource) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(closestResource);
-                return
+                return;
             }
-    
             creep.harvest(closestResource);
-        } else {
+        }
+        else {
+            // @ts-expect-error
             creep.memory.working = true;
         }
-
         if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0) {
+            // @ts-expect-error
             creep.memory.working = false;
         }
-    
+        // @ts-expect-error
         if (creep.memory.working) {
             transferToStructure(creep);
         }
     }
-}
-
-const transferToStructure = (creep) => {
-    const structuresWithEnergyNeeds = creep.room.find(FIND_STRUCTURES, {
-        filter: (structure) => ( structure.structureType === STRUCTURE_SPAWN ) && structure.store.getUsedCapacity(RESOURCE_ENERGY) < structure.store.getCapacity(RESOURCE_ENERGY)
+};
+var transferToStructure = function (creep) {
+    var structuresWithEnergyNeeds = creep.room.find(FIND_STRUCTURES, {
+        filter: function (structure) { return (structure.structureType === STRUCTURE_SPAWN) && structure.store.getUsedCapacity(RESOURCE_ENERGY) < structure.store.getCapacity(RESOURCE_ENERGY); }
     });
-
     if (structuresWithEnergyNeeds.length) {
         if (creep.transfer(structuresWithEnergyNeeds[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
             creep.moveTo(structuresWithEnergyNeeds[0]);
             return;
         }
-
         creep.transfer(structuresWithEnergyNeeds[0], RESOURCE_ENERGY);
-    } else {
+    }
+    else {
         if (creep.transfer(creep.room.controller, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
             creep.moveTo(creep.room.controller);
         }
-
         creep.transfer(creep.room.controller, RESOURCE_ENERGY);
     }
-}
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
-const { task, src } = require('gulp')
+const { task, src, dest } = require('gulp')
 const screeps = require('gulp-screeps')
+const ts = require('gulp-typescript')
+
 const credentials = require('./credentials')
 
+task('typescript', () => {
+    return src('src/**/*.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+            outFile: 'main.js'
+        }))
+        .pipe(dest('build'))
+})
+
 task('deploy', () => {
-    return src('src/*.js')
+    return src('build/*.js')
         .pipe(screeps(credentials))
 })
