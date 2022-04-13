62b7ec3897b2b170df5c8a1b54cbb968c23f6adc
Mart Linger <martlinger@gmail.com>
2022-04-13 22:47:47 +0200

    Basic harvester logic implemented

--- src/main.js
+++ src/main.js
@@ -1,3 +1,46 @@
 module.exports.loop = () => {
-    // This is a test
+    for (const i in Game.creeps) {
+        const creep = Game.creeps[i];
+        const closestResource = creep.pos.findClosestByPath(FIND_SOURCES);
+        
+        if (creep.store.getFreeCapacity() > 0 && !creep.memory.working) {
+            if (creep.harvest(closestResource) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(closestResource);
+                return
+            }
+    
+            creep.harvest(closestResource);
+        } else {
+            creep.memory.working = true;
+        }
+
+        if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0) {
+            creep.memory.working = false;
+        }
+    
+        if (creep.memory.working) {
+            transferToStructure(creep);
+        }
+    }
+}
+
+const transferToStructure = (creep) => {
+    const structuresWithEnergyNeeds = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => ( structure.structureType === STRUCTURE_SPAWN ) && structure.store.getUsedCapacity(RESOURCE_ENERGY) < structure.store.getCapacity(RESOURCE_ENERGY)
+    });
+
+    if (structuresWithEnergyNeeds.length) {
+        if (creep.transfer(structuresWithEnergyNeeds[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(structuresWithEnergyNeeds[0]);
+            return;
+        }
+
+        creep.transfer(structuresWithEnergyNeeds[0], RESOURCE_ENERGY);
+    } else {
+        if (creep.transfer(creep.room.controller, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(creep.room.controller);
+        }
+
+        creep.transfer(creep.room.controller, RESOURCE_ENERGY);
+    }
 }
